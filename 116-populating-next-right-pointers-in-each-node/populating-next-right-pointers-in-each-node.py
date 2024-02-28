56f7f092f6a76d251c070eb12b4ab1ff1001acd1
XuejunZhao <xuejunzhao0423@gmail.com>
2024-02-28 23:55:19 +0800

    Time: 55 ms (26.35%) | Memory: 18 MB (83.37%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -1,34 +1,30 @@
 """
 # Definition for a Node.
-class Node(object):
-    def __init__(self, val=0, left=None, right=None, next=None):
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
         self.val = val
         self.left = left
         self.right = right
         self.next = next
 """
 
-class Solution(object):
-    def connect(self, root):
-        """
-        :type root: Node
-        :rtype: Node
-        """
-        if not root: return root 
-        curr = root 
-        res = [curr]
-        while res: 
-            n = len(res)
-            # print (n)
-            for i in range(n-1):
-                res[i].next = res[i+1]
-            for i in range(n):
-                curr = res.pop(0)
+class Solution:
+    def connect2nodes(self, left: 'Optional[Node]', right: 'Optional[Node]') -> 'Optional[Node]':
+        if not left: return right 
+        if not right: return left 
+        left.next = right
+        return left, right
 
-                if curr.left: 
-                    res.append(curr.left)
-                if curr.right: 
-                    res.append(curr.right)
-     
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root: 
+            return root
+        left = root.left
+        right = root.right
+        self.connect2nodes(left,right)
+
+        if root.next != None and right: 
+            self.connect2nodes(right, root.next.left)
+        self.connect(left)
+        self.connect(right)
+        
         return root
-
