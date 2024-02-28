89f12c2a3f4db6a21cfc5a261c25ca92255fe86c
XuejunZhao <xuejunzhao0423@gmail.com>
2024-02-29 00:46:45 +0800

    Time: 1991 ms (9.31%) | Memory: 25.8 MB (30.92%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,45 +1,44 @@
-class Solution(object):
-    def sortArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        import random
-        # # nums
-        if nums == sorted(nums): 
-            return nums
-        nums = sorted(nums, key=lambda x: random.random())
-        n = len(nums)
-        if n <= 1: 
-            return nums 
-        # pivot = nums[0]
-        # l = 0
-        # for i in range(1,n):
-        #     if nums[i] < pivot: 
-        #         l += 1
-        #         nums[i], nums[l] = nums[l], nums[i]
-         
-        # nums[0], nums[l] = nums[l], nums[0]
-        # nums[0:l] = self.sortArray(nums[0:l])
-        # nums[l+1:] = self.sortArray(nums[l+1:])
-        # return nums
+class Solution:
+    def quicksort(self, nums: List[int], start: int, end: int) -> List[int]:
+        
+        if start >= end - 1: return nums 
+        pivot = nums[start]
+        pivot_left = start + 1
+        for i in range(pivot_left, end):
+            if nums[i] < pivot: 
+                nums[i], nums[pivot_left] = nums[pivot_left], nums[i]
+                pivot_left += 1
+        nums[pivot_left-1], nums[start] = nums[start], nums[pivot_left-1]
+        nums = self.quicksort(nums, start, pivot_left-1)
+        nums = self.quicksort(nums, pivot_left, end)
+        return nums
 
-        mid = n / 2
-        l = self.sortArray(nums[:mid])
-        r = self.sortArray(nums[mid:])
+    def mergesort(self, nums: List[int])  -> List[int]:
+        
+        n = len(nums)
+        if n <= 1: return nums
+        start = 0 
+        end = n 
+        mid = start + (end-start)//2
+        nums1 = self.mergesort(nums[start: mid])
+        nums2 = self.mergesort(nums[mid: end])
         res = []
-        while l and r: 
-            if l[0] <= r[0]:
-                res.append(l[0])
-                l.pop(0)
+        while nums1 and nums2: 
+            if nums1[0] < nums2[0]:
+                res.append(nums1.pop(0))
             else:
-                res.append(r[0])
-                r.pop(0)
-        if l: res+=l
-        if r: res+=r
+                res.append(nums2.pop(0))
+
+        if nums1: res += nums1
+        if nums2: res += nums2
         return res
 
 
-                
-        
-        
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # random.shuffle(nums)
+        # n = len(nums)
+        # start = 0 
+        # end = n 
+        # nums = self.quicksort(nums, start, end)
+        nums = self.mergesort(nums)
+        return nums
