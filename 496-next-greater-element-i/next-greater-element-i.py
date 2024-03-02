0f1fd5d9c1894fc5fdcca35da53f9aea83cb2834
XuejunZhao <xuejunzhao0423@gmail.com>
2024-03-02 15:19:14 +0800

    Time: 39 ms (47.00%) | Memory: 11.8 MB (75.53%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,26 +1,27 @@
-class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack = []
-        nums2_dict = {}
-        n = len(nums2)
-        for i in range(n-1, -1, -1): 
-            num = nums2[i]
-            if stack and stack[-1] > num:
-                 nums2_dict[num] =  stack[-1]
-            else:
-                while stack and stack[-1] <= num:
-                    stack.pop(-1) 
-                if stack: nums2_dict[num] =  stack[-1]
-                else: nums2_dict[num] = -1 
-            stack.append(num)
-
-        res = []
-        for num in nums1:
-            if num in nums2_dict.keys():
-                res.append(nums2_dict[num])
-            else:
-                res.append(-1)
-        return res
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        out = []
+        n2 = len(nums2)
+        big_nums2 = [-1]*n2
+        stk = [nums2[-1]]
+        for j in range(n2-2, -1, -1):
+            # print (stk)
             
+            if nums2[j] >= stk[-1]:
+                while stk and nums2[j] >= stk[-1]:
+                    stk.pop(-1)
+            if stk: big_nums2[j] = stk[-1]
+            stk.append(nums2[j])
 
-            
+        for num1 in nums1:
+            num1_idx = nums2.index(num1)
+            out.append(big_nums2[num1_idx])
+        return out
+                
+
+        
