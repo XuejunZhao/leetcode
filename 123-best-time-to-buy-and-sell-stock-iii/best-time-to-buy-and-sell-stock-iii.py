bb09891ad1be0b3dc0e9f6089254408db9228995
XuejunZhao <xuejunzhao0423@gmail.com>
2024-03-04 14:37:46 +0800

    Time: 1073 ms (24.08%) | Memory: 24.9 MB (45.31%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -10,11 +10,11 @@ class Solution(object):
         dp = [[0 for i in range(n)] for j in range(k+1)]
         for j in range(1,k+1):
             for i in range(n):
-                if i == 0: 
-                    dp[j][i] = 0 
-                else:
-                    dp[j][i] = max(dp[j-1][i], dp[j][i-1])
-                    if i > 0: dp[j][i] = max(dp[j][i], prices[i]+balance_if_buy)
+                # if i == 0: 
+                #     dp[j][i] = 0 
+                # else:
+                dp[j][i] = max(dp[j-1][i], dp[j][i-1])
+                if i > 0: dp[j][i] = max(dp[j][i], prices[i]+balance_if_buy)
 
                 balance_if_buy = max(dp[j-1][i-1] - prices[i], balance_if_buy) if i>0 else -prices[i]
         return dp[k][n-1]
