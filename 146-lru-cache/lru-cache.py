4cc88067be0fbdfd7daf04c1b52f05fa5e6cd708
XuejunZhao <xuejunzhao0423@gmail.com>
2024-03-04 12:41:58 +0800

    Time: 653 ms (12.15%) | Memory: 78.9 MB (11.86%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,70 +1,83 @@
 class LRUCache:
     class LinkNode:
-        def __init__(self,key=0,val=0, pre=None, next=None):
+        def __init__(self,key=0,val=0, prev=None, next=None):
             self.key = key 
             self.val = val 
-            self.pre = pre 
+            self.prev = prev 
             self.next = next 
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}
-        self.cacheTail = None
-        self.cacheHead = None
-        # self.cache = collections.OrderedDict()
+        self.cap = capacity
+        self.tb = dict()
+        # delete the head: most recently used
+        self.Head = None 
+        # new add to tail
+        self.Tail = None 
+  
     def addNode(self, key, value):
         node = self.LinkNode(key, value)
-        if self.cacheTail: 
-            node.pre = self.cacheTail
-            self.cacheTail.next = node  
-            self.cacheTail = node 
-        else: 
-            self.cacheTail = node 
-            self.cacheHead = node
-        self.cache[key] = node
-    
+        self.tb[key]=node
+        if not self.Tail: 
+            self.Head = node 
+            self.Tail = node 
+        else:
+            self.Tail.next = node
+            node.prev = self.Tail
+            self.Tail = node
+
     def deleteNode(self, key):
-        node = self.cache[key]
-        if self.cacheHead.key == key: 
-            self.cacheHead = self.cacheHead.next
-        if self.cacheTail.key == key: 
-            self.cacheTail = self.cacheTail.pre
-        if node.pre: 
-            node.pre.next = node.next 
+        node = self.tb[key]
+        del self.tb[key]
+        if node == self.Tail: 
+            self.Tail = node.prev
+        if node == self.Head:
+            self.Head = node.next 
         if node.next: 
-            node.next.pre = node.pre
+            node.next.prev = node.prev
+        if node.prev: 
+            node.prev.next = node.next 
+        
         
     def popNode(self):
-        key = self.cacheHead.key
-        self.cacheHead = self.cacheHead.next 
-        self.cacheHead.pre = None 
-        del self.cache[key]
-
+        node = self.Head
+        key = node.key
+        del self.tb[key]
+        # print (key)
+        # print (self.Tail.key)
+        if node == self.Tail: 
+            self.Tail = None 
+            self.Head = None
+        else:
+            self.Head = node.next
+            if node.next: 
+                self.Head.prev = None
+            print (self.Head.key)
     
     def get(self, key: int) -> int:
-
-        if key in self.cache.keys():
-            value = self.cache[key].val
-            if self.cacheTail.key != key:
-                self.deleteNode(key)
-                self.addNode(key, value)
+        if key in self.tb.keys(): 
+            node = self.tb[key]
+            self.deleteNode(key)
+            value = node.val
+            self.addNode(key, value)
             return value
         else: 
-            return -1  
-        
+            return -1
 
     def put(self, key: int, value: int) -> None:
-
-        if key in self.cache.keys():
-            # self.cache.move_to_end(key)
-            self.deleteNode(key)
-        self.addNode(key, value)
-
-        if len(self.cache) > self.capacity:
+        # print (key)
+        # print (self.tb.keys())
+        if key not in self.tb.keys() and len(self.tb) < self.cap : 
+            self.addNode(key, value)
+        elif key not in self.tb.keys() and len(self.tb) >= self.cap : 
             self.popNode()
-        
-        
-        
+            self.addNode(key, value)
+        else: 
+            node = self.tb[key]
+            self.deleteNode(key)
+            self.addNode(key, value)
+        # print (self.tb.keys())
+
+
 
 
 # Your LRUCache object will be instantiated and called as such:
