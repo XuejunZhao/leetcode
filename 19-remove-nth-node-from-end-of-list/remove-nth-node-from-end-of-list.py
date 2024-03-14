bd3cbdf52f5c62e6a11a2a18fa745c4b112aeeec
XuejunZhao <xuejunzhao0423@gmail.com>
2024-03-14 13:55:37 +0800

    Time: 38 ms (47.57%) | Memory: 16.7 MB (11.88%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,40 +1,28 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        l = None 
-        r = head 
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # locate n+1 
+        # slow, fast has the gap with n+1 
+        fast = head 
         gap = n + 1
-        while gap and r: 
-            
-            r = r.next 
+        while fast and gap: 
+            fast = fast.next 
             gap -= 1
-        # if not r: return head
-        if gap > 1 and not r: 
-            return None 
-        elif gap == 1 and not r: 
-            return head.next
+        if not fast and gap == 1: 
+                return head.next 
+        if not fast and gap > 1: 
+                return None 
         else:
-            l = head
-            while r:
-                l = l.next
-                r = r.next 
-        
-            l_next = l.next
-            l.next = l_next.next 
-        return head
-
-
-
-                
-
+            slow = head 
+            while fast: 
+                slow = slow.next 
+                fast = fast.next 
+            
+            slow.next = slow.next.next 
+            return head 
 
         
