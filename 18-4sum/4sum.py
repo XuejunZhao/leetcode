fbc0c16741d79a3bcdf81afd09efa9062cb194b9
XuejunZhao <xuejunzhao0423@gmail.com>
2024-03-18 22:34:08 +0800

    Time: 372 ms (69.92%) | Memory: 16.6 MB (88.83%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,42 +1,47 @@
-class Solution(object):
-    def kSum(self, nums, target, k_size):
-        n = len(nums)
-        l = 0 
-        r = n - 1
+class Solution:
+    def Sum(self, candidates, target, n):
         res = []
-        if k_size == 2:
+        length = len(candidates)
+        if n == 2:
+            l = 0 
+            r = length - 1
             while l < r: 
-                if nums[l] + nums[r] == target: 
-                    res.append([nums[l],nums[r]])
-                    pre_l, post_r = l, r
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[pre_l]: 
-                        l += 1
-                    while l < r and nums[r] == nums[post_r]: 
-                        r -= 1
-                elif nums[l] + nums[r] > target:
-                    r -= 1
+                if candidates[l] + candidates[r] < target:
+                    l += 1 
+                elif candidates[l] + candidates[r] > target:
+                    r -= 1 
                 else: 
-                    l += 1
-            return res 
-        elif k_size >= 3: 
-            for i, num in enumerate(nums):
-                if i >= n - k_size+1 or (i > 0 and num == nums[i-1]): continue
-                sub_res = self.kSum(nums[i+1:],target-num,k_size-1)
-                if len(sub_res) ==0: continue
-                for sub_sum in sub_res: 
-                    res.append([nums[i]]+sub_sum)
-            return res
-        
-
-
-    def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        nums.sort()
-        res = self.kSum(nums, target, 4)
-            
+                    res.append([candidates[l], candidates[r]])
+                    last_l = candidates[l]
+                    last_r = candidates[r]
+                    while l < r and candidates[l] == last_l:
+                        l += 1
+                    while l < r and candidates[r] == last_r:
+                        r -= 1
+        elif n >= 3: 
+            last = float('inf')
+            for i,c in enumerate(candidates[:-n+1]):
+                if c == last: continue
+                Sum2 = self.Sum(candidates[i+1:], target-c, n-1)
+                for sub in Sum2:
+                    res.append([c]+sub)
+                last = c
+        # elif n == 4: 
+        #     last = float('inf')
+        #     for i,c in enumerate(candidates[:-3]):
+        #         if c == last: continue
+        #         Sum2 = self.Sum2(candidates[i+1:], target-c, 3)
+        #         for sub in Sum2:
+        #             res.append([c]+sub)
+        #         last = c
         return res
+
+    def fourSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        n = len(candidates)
+        candidates.sort()
+        
+        Sum2 = self.Sum(candidates, target,4)
+        
+        
+        return Sum2
